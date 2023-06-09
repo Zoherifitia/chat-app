3407096d7c3245d783db3b62de7665bacf434c8f
zoherifitia <hei.nitsiaro@gmail.com>
2023-06-10 00:27:10 +0300

    chore: profile user

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,8 @@
 import BASE_URL from "@/api/BASE_URL";
+import { putUser } from "@/api/user";
 import axios from "axios";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 
 export const getServerSideProps = async (context: any) => {
   const token = context.req.cookies.token;
@@ -32,11 +35,31 @@ export const getServerSideProps = async (context: any) => {
 };
 
 const Profile = ({ token, userLoged }: { token: string; userLoged: any }) => {
-  const user = userLoged.user;
+  const {register,handleSubmit} = useForm();
+  const user = userLoged.user;  
+
+  const updateSubmit = async (logObject : any) => {
+    await putUser(token,logObject);
+  }
   return (
     <>
-      <p>{JSON.stringify(user)}</p>
-      <p>{user.token}</p>
+    <div>
+      <p>id : {user.id}</p>
+      <p>name : {user.name}</p>
+      <p>email : {user.email}</p>
+      
+    </div>
+    <div>
+      <h4>Edit profile</h4>
+      <form onSubmit={handleSubmit(updateSubmit)}>
+        <label htmlFor="">Name</label><input type="text" {...register("name")} />
+        <label htmlFor="">Email</label><input type="email" disabled value={user.email}/>
+        <label htmlFor="">Current password</label><input type="password" {...register("currentPassword")} />
+        <label htmlFor="">New password</label><input type="password" {...register("newPassword")} />
+        <label htmlFor="">Confirm password</label><input type="password" {...register("confirmPassword")} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
     </>
   );
 };
